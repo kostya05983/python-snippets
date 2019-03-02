eefeb13c246ff0d1e2163e69acfe753ecb4c3c94
kostya05983 <kostya05983@mail.ru>
2019-03-02 10:36:29 +0700

    Add cgi example

---
+++ cockieCgi/example1.py
@@ -0,0 +1,21 @@
+import Cookie
+
+cook = Cookie.SimpleCookie()
+cook["UserID"] = "Frahaan"
+cook["UserID"]["expires"] = 60*60*24
+#Deleting a Cookkue
+# cook["UserID"]["expires"] = ""
+
+print(cook)
+
+print("Content-type: text/html\r\n\r\n")
+
+print("<html>")
+print("<head>")
+print("<title>My First CGI App</title>")
+print("</head>")
+print("<body>")
+print("<h3>This is HTML's Body section</h3>")
+print(cook["UserID"].value)
+print("</body>")
+print("</html>")
